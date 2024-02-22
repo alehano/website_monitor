30fdef07a56fb412c90b97b0fdbdb2a2d24851a5
Alex <halyapin@gmail.com>
2024-02-22 12:27:07 +0000

    add headers and rnd delay

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
@@ -22,15 +23,34 @@ var (
 )
 
 func fetchPageContent(url string) (string, error) {
-	resp, err := http.Get(url)
+	// Create a new request
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	// Add headers to the request
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+	req.Header.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Add("Accept-Language", "en-US,en;q=0.5")
+	req.Header.Add("DNT", "1")
+	req.Header.Add("Connection", "keep-alive")
+	req.Header.Add("Upgrade-Insecure-Requests", "1")
+
+	// Send the request
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
+
+	// Read the response body
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
+
 	return string(body), nil
 }
 
@@ -82,6 +102,10 @@ func main() {
 	checkPageForText() // initial check
 
 	for range ticker.C {
+		// Generate a random delay of 5-10% of the checkInterval
+		delay := time.Duration(rand.Int63n(int64(checkInterval)/10-int64(checkInterval)/20) + int64(checkInterval)/20)
+		time.Sleep(delay)
+
 		checkPageForText()
 	}
 }
